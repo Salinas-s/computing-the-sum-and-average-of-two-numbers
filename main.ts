a154a8315578ffdbf2a2d02678f887b6606b0c6a
Salinas-s <142606289+Salinas-s@users.noreply.github.com>
2023-09-12 10:19:01 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let minval = Math.min(2, 9)
+game.splash("The minimum value is " + minval)
+let remainder = 4 % 2
+if (remainder == 0) {
+    game.splash("The integer is " + "evenly divisible")
+} else {
+    game.splash("The remainder is " + remainder)
+}
